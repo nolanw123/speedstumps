485830343abf07ca2a651713692937d6fedaa034
William Nolan <will@landale.net>
2022-11-16 15:14:53 +0800

    * Added various things

--- util.h
+++ util.h
@@ -7,19 +7,3 @@ inline int64_t get_ts()
   return static_cast<int64_t>(tp.tv_sec) * 1000 * 1000 * 1000 + static_cast<int64_t>(tp.tv_nsec);
 }
 
-// treating IT as an array of type T, return get the I'th element
-//
-// i.e.:
-//
-// __m128i foo;
-// return VGBI<uint32_t, 3>(foo); // get 4th uint32_t from foo
-//
-template<typename T, size_t I, typename IT>
-T VGBI(IT V) {
-  union {
-    IT v;
-    T a[4];
-  } converter;
-  converter.v = V;
-  return converter.a[I];
-}
--- vectest.cc
+++ vectest.cc
@@ -16,7 +16,6 @@
 //
 // we could do this in parallel with simd instructions, and avoid branching entirely
 //
-//
 // the instrinsics corresponding to the "basic idea" above are:
 // https://software.intel.com/sites/landingpage/IntrinsicsGuide/#techs=AVX&expand=486,518,848,848&text=_mm_cmp_ps
 //
@@ -25,13 +24,12 @@
 // https://software.intel.com/sites/landingpage/IntrinsicsGuide/#techs=SSE4_1&expand=486,518&text=_mm_blendv_ps
 //
 // handy note:
+//
 // we can get an annotated dump like this:
 // objdump -d -M intel -S objs/opt/vectest.o  > vectest.asm
 //
 
-#include <stdint.h>
 #include <immintrin.h>
-#include <xmmintrin.h>
 
 #include <iostream>
 #include <random>
@@ -40,7 +38,7 @@
 
 inline float horizontal_add(__m256 &a) {
   a = _mm256_hadd_ps(a,a);
-  a = _mm256_hadd_ps(a, a);
+  a = _mm256_hadd_ps(a,a);
   __m128 t1 = _mm256_extractf128_ps(a,1);
   t1 = _mm_add_ss(_mm256_castps256_ps128(a),t1);
   return _mm_cvtss_f32(t1);
@@ -103,74 +101,23 @@ float selectslow(float *a, float *b, float *x, float *y, size_t count)
   return total / count;
 }
 
-// 16-bit aligned a/b, mask, and results array, do selection for count (which must be a multiple of 4)
-// this version uses the idea of keeping the arguments to _mm_blendv_ps next to each other in groups
-// -- some experiments with cachegrind showed that keeping the result in the struct (which makes
-// the struct 64 bytes, the size of a cache line) result in maybe 1/4 missed cache writes (why?)
-// -- keeping the results in their own array was faster.  With this we get results that are
-// roughly 70% of the selectf2 numbers.  48/64 is 75% which is interesting.
-#pragma pack(push, 1)
-struct selectf2_2_entry {
-  __m128 a,b,mask;
-};
-static_assert(sizeof(selectf2_2_entry) == 48, "Size of selectf2_2_entry is not 48 bytes");
-#pragma pack(pop)
-
-float selectf2_2(selectf2_2_entry *entries, size_t count)
-{
-  __m128 tot;
-  
-  for(size_t i = 0 ; i < (count >> 2) ; ++i) {
-    __m128 res = _mm_blendv_ps(entries->a, entries->b, entries->mask);
-    tot = _mm_add_ps(tot, res); // vertically accumulate results    
-    ++entries;
-  }
-
-  tot = _mm_hadd_ps(tot, tot); // now horizontally accumulate results
-  tot = _mm_hadd_ps(tot, tot); // need to do 2x because hadd_ps works in pairs
-
-  return VGBI<float, 0>(tot) / count;  
-}
-
 int main(int argc, char **argv)
 {
   const size_t TRIALS = 200;
-  
   const size_t COUNT = 8*100000;
 
-  __m256 *mask = new __m256[COUNT/8]; //{ 0xffffffff,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0xffffffff};
-  mask[0][0] = 0xffffffff;
-  mask[5][0] = 0xffffffff;  
-
-  if(argv[1][0] == 'a') {
-    mask[1][0] = 0xffffffff;
-  }
-
   __m256 *a = new __m256[COUNT/8];
-  __m256 *b = new __m256[COUNT/8];  
+  __m256 *b = new __m256[COUNT/8];
   __m256 *x = new __m256[COUNT/8];
   __m256 *y = new __m256[COUNT/8];
-  selectf2_2_entry *entries2 = new selectf2_2_entry[COUNT/4]; // /4 because using __m128 internally
 
   std::mt19937_64 g(1234);      // mersenne twister with constant seed for reproducibility
   std::uniform_real_distribution<float> d(-0.1, 0.1);
   for(size_t i = 0 ; i < COUNT/8 ; ++i) {
-    {
-      float vals[8] = { d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g) };
-      a[i] = _mm256_set_ps(vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7]);
-    }
-    {
-      float vals[8] = { d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g) };
-      b[i] = _mm256_set_ps(vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7]);
-    }
-    {
-      float vals[8] = { d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g) };
-      x[i] = _mm256_set_ps(vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7]);
-    }
-    {
-      float vals[8] = { d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g) };
-      y[i] = _mm256_set_ps(vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7]);
-    }    
+    a[i] = _mm256_set_ps(d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g));
+    b[i] = _mm256_set_ps(d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g));
+    x[i] = _mm256_set_ps(d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g));
+    y[i] = _mm256_set_ps(d(g), d(g), d(g), d(g), d(g), d(g), d(g), d(g));
   }
     
   std::cout << "Running tests on " << COUNT << " elements" << std::endl;
@@ -192,7 +139,6 @@ int main(int argc, char **argv)
   timer([&](){ return selectslow(&a[0][0],&b[0][0],&x[0][0],&y[0][0],COUNT); }, TRIALS, "selectslow");
   timer([&](){ return selectf(&a[0][0],&b[0][0],&x[0][0],&y[0][0],COUNT); }, TRIALS, "selectf");
   timer([&](){ return selectf2(&a[0][0],&b[0][0],&x[0][0],&y[0][0],COUNT); }, TRIALS, "selectf2");
-  timer([&](){ return selectf2_2(&entries2[0],COUNT); }, TRIALS, "selectf2_2");
 
   return 0;
 }
